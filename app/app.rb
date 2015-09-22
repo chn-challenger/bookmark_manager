074799cf2999e1d87c4781f0515f926b9249ef77
chn.challenger <chn.challenger@gmail.com>
2015-09-22 12:27:42 +0100

    first test passing

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,12 @@
 require 'sinatra/base'
 require_relative 'models/link.rb'
+require_relative '../data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
   set :views, proc{File.join(root, '..' , 'views')}
 
-  get '/' do
-    erb :index
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,9 +1,11 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+
 class Link
   include DataMapper::Resource
 
-  property :id,     Serial
-  property :title,  String
-  property :url,    String
+  property :link_id,     Serial
+  property :url,         String
+  property :title,       String
+
 end
