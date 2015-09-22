d9591be9159eb1792c089e36be231884eef0310b
chn.challenger <chn.challenger@gmail.com>
2015-09-22 16:03:16 +0100

    redirect homepage

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,10 @@ require_relative '../data_mapper_setup'
 class BookmarkManager < Sinatra::Base
   set :views, proc{File.join(root, '..' , 'views')}
 
+  get '/' do
+    redirect '/links'
+  end
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
